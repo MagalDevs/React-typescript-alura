cefc1ed002f7529990280684b3979b57fe0394b5
MagalDevs <danielgalfla12@gmail.com>
2025-02-23 17:16:56 -0300

    deletando files e criando componentes

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Botao extends Component {
+    render() {
+        return (
+            <button>
+                botão da massa
+            </button>
+        );
+    }
+}
+
+export default Botao;
